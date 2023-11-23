93cf7ab34ffa76e72caf74a7296d668ac079588b
Neeharica Madanu <146192110+NMadanu-L00179439@users.noreply.github.com>
2023-11-23 19:22:02 +0000

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -2,6 +2,15 @@ resource "aws_instance" "PublicEC2" {
   ami =   "ami-005e7be1c849abba7"
   instance_type = "t2.micro"
   vpc_security_group_ids = ["${aws_security_group.allow_ssh.id}"]
+  user_data = <<EOF
+sudo yum update -y
+sudo amazon-linux-extras install docker
+sudo service docker start
+sudo usermod -a -G docker ec2-user
+mkdir datadocker
+cd datadocker/
+
+EOF
   subnet_id = "${aws_subnet.PublicSubnet_A.id}"
   key_name = "ec2creationkey"
   tags = {
